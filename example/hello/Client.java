e34645ed7c44ba2211cf1d42e7ed8e80c1c0b69a
Pedro Lucas <102874758+Chazer006@users.noreply.github.com>
2026-04-26 23:41:27 -0300

    Refactor Client to test new Hello and Conversor servers
    Updated client to use new server methods and removed unused code.

--- example/hello/Client.java
+++ example/hello/Client.java
@@ -1,32 +1,27 @@
 package example.hello;
 
 import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class Client {
 
-    private Client() {}
-
     public static void main(String[] args) {
-
-        System.out.println("Initiating client");
+        String host = (args.length < 1) ? "localhost" : args[0];
         
-        String host = (args.length < 1) ? null : args[0];
         try {
-            //Registry registry = LocateRegistry.getRegistry(host);
-            //System.out.println("Registry has been located");
-            //Hello stub = (Hello) registry.lookup("Hello");
-
-            Hello stub = (Hello) Naming.lookup("rmi://" + host + "/MyHello"); 
-            System.out.println("Found server");
-            String response = stub.sayHello();
-            System.out.println("Response: " + response);
-
-            int result = stub.soma(100,1000);
-            System.out.println("Response from soma: " + result);
+            System.out.println("--- Testando Servidor Hello ---");
+            Hello stubHello = (Hello) Naming.lookup("rmi://" + host + "/MyHello"); 
             
+            System.out.println("Soma (10+5): " + stubHello.soma(10, 5));
+            System.out.println("Subtração (20-8): " + stubHello.subtrai(20, 8));
+            System.out.println("Maiúsculo: " + stubHello.paraMaiusculo("java rmi funcional"));
+
+            System.out.println("\n--- Testando Novo Servidor Conversor ---");
+            Conversor stubConv = (Conversor) Naming.lookup("rmi://" + host + "/MeuConversor");
+            
+            double valorDolar = 50.0;
+            System.out.println("Valor em Dólar: $" + valorDolar);
+            System.out.println("Conversão para Real: R$ " + stubConv.dolarParaReal(valorDolar));
+
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
